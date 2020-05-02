8201ede230f703aff1f688ad9a64df70ec1f1c19
exinmusic <exinmusic@gmail.com>
2020-05-01 17:27:05 -0700

    production compose

--- pp/pp/settings.py
+++ pp/pp/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'pp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if not os.environ.get('PROD'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'pp_base',
+            'USER': 'pp_worker',
+            'PASSWORD': 'safety2ndplacech@mp',
+            'HOST': 'db',
+            'PORT': 5432,
+        }
+    }
 
 
 # Password validation
