4e39d78fbb5e764170a1ea6dbdb6ee733d115f67
xn <exinmusic@gmail.com>
2020-07-19 02:27:54 -0700

    SSL plz

--- pp/pp/settings.py
+++ pp/pp/settings.py
@@ -18,6 +18,11 @@ SECRET_KEY = private.key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
 ALLOWED_HOSTS = [
     '127.0.0.1',
     '178.128.190.190',
