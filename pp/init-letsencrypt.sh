ebd85340f8c85ea4853b25e823316e614d4ee735
xn <exinmusic@gmail.com>
2020-07-18 23:02:20 -0700

    Update init-letsencrypt.sh

--- pp/init-letsencrypt.sh
+++ pp/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(example.org www.publicpasta.com)
+domains=(www.publicpasta.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="exinmusic@gmail.com" # Adding a valid address is strongly recommended
